56ac7dec5bab11e99a42c71f64ea8b5e233a95ba
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-05 16:16:54 +0500

    add

--- OnlineBazarWebAPI/Extensions/Middleware.cs
+++ OnlineBazarWebAPI/Extensions/Middleware.cs
@@ -1,5 +1,14 @@
 ﻿using Bazar.DataAcces.AppDBContexts;
+using Bazar.DataAcces.IRepository;
+using Bazar.DataAcces.Repository;
+using Bazar.Domain.Entities.Collectors;
+using Bazar.Domain.Entities.Others;
+using Bazar.Domain.Entities.Taxi;
+using Bazar.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace OnlineBazarWebAPI.Extensions
 {
@@ -9,6 +18,18 @@ namespace OnlineBazarWebAPI.Extensions
         {
             services.AddDbContext<AppDbContext>(options =>
                  options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        }
+        public static void AddServices(this IServiceCollection collection)
+        {
+
+            //Repositories
+            collection.AddTransient<IGenericRepository<User>, GenericRepository<User>>();
+            collection.AddTransient<IGenericRepository<Order>, GenericRepository<Order>>();
+            collection.AddTransient<IGenericRepository<Car>, GenericRepository<Car>>();
+            collection.AddTransient<IGenericRepository<Driver>, GenericRepository<Driver>>();
+            collection.AddTransient<IGenericRepository<Orders>, GenericRepository<Orders>>();
+            collection.AddTransient<IGenericRepository<Collector>, GenericRepository<Collector>>();
+            collection.AddTransient<IGenericRepository<Address>, GenericRepository<Address>>();
         }
     }
 }
--- OnlineBazarWebAPI/Program.cs
+++ OnlineBazarWebAPI/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddDBConTextes(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddServices();
 
 
 
