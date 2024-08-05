5c0c832bbd0bc1149549d399dc8dffc69cec835f
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-05 10:04:50 +0500

    add

---
+++ Bazar.Domain/Entities/Collectors/Collector.cs
@@ -0,0 +1,13 @@
+﻿using Bazar.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Domain.Entities.Collectors
+{
+    public class Collector : Auditable
+    {
+    }
+}
---
+++ Bazar.Domain/Entities/Others/Address.cs
@@ -0,0 +1,14 @@
+﻿using Bazar.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Domain.Entities.Others
+{
+    public class Address : Auditable
+    {
+
+    }
+}
