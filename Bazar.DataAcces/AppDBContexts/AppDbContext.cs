820707fd9cda9980ee8e8a0df11803558f8f6e5f
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-05 10:57:09 +0500

    ADD

---
+++ Bazar.DataAcces/AppDBContexts/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Bazar.Domain.Entities.Collectors;
+using Bazar.Domain.Entities.Others;
+using Bazar.Domain.Entities.Taxi;
+using Bazar.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.DataAcces.AppDBContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { }
+
+        public DbSet<Collector> collectors { get; set; }
+
+        public DbSet<Address> addresses { get; set; }
+
+        public DbSet<Car> cars { get; set; }
+
+        public DbSet<Driver> drivers { get; set; }
+
+        public DbSet<Orders> DriverOrder { get; set; }
+
+        public DbSet<Order> UserOrder { get; set; }    
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- OnlineBazarWebAPI/Program.cs
+++ OnlineBazarWebAPI/Program.cs
@@ -1,11 +1,17 @@
+using OnlineBazarWebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDBConTextes(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
