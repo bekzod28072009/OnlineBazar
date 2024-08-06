012fb010886e0184cfcf2f9547d2ab03c9cc3e64
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-06 10:44:27 +0500

    add

---
+++ Bazar.Service/Exeptions/OnlineBazarExeption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Service.Exeptions
+{
+    public class OnlineBazarExeption : Exception
+    {
+        public int Code { get; set; }
+        public OnlineBazarExeption(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ Bazar.Service/Service/UserService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Bazar.DataAcces.IRepository;
+using Bazar.Domain.Entities.Users;
+using Bazar.Service.DTOs.Users;
+using Bazar.Service.Exeptions;
+using Bazar.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Service.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IGenericRepository<User> repository;
+        private readonly IMapper mapper;
+
+        public UserService(IGenericRepository<User> repository,
+            IMapper mapper)
+        {
+            this.repository = repository;   
+            this.mapper = mapper;
+        }
+        public async ValueTask<UserForViewDto> CreateAsync(UseerForCreateDto entity)
+        {
+            if (entity.Password != entity.ConfirmPassword)
+                throw new OnlineBazarExeption(404, "Passwords are different");
+
+            var existEmail = await repository.GetAsync(u => u.Email == entity.Email);
+
+
+            if (existEmail != null)
+                throw new OnlineBazarExeption(400, "This email is already taken");
+
+            var createdUser = await repository.CreateAsync(mapper.Map<User>(entity));
+
+            createdUser.Password = createdUser.Password.
+
+            await repository.SaveChangesAsync();
+
+            return mapper.Map<UserForViewDto>(createdUser);
+        }
+
+        public ValueTask<bool> DeleteAsync(Expression<Func<User, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserForViewDto> GetAll(Expression<Func<User, bool>> expression = null, string[] includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserForViewDto> GetAsync(Expression<Func<User, bool>> expression, string[] includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserForViewDto> Update(long id, UserForUpdateDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
