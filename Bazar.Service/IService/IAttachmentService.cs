f9de52a50c79edec9f8c0f542a8021a75fb7c66f
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-23 16:23:25 +0500

    add

---
+++ Bazar.Service/IService/IAttachmentService.cs
@@ -0,0 +1,20 @@
+﻿using Bazar.Domain.Entities.Commons;
+using Bazar.Service.DTOs.Attachments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Service.IService
+{
+    public interface IAttachmentService
+    {
+        ValueTask<Attachment> UploadAsync(AttachmentForCreateDto dto);
+        ValueTask<Attachment> UploadToCTFAsync(AttachmentForCreateDto dto, long Id);
+        ValueTask<Attachment> UploadToGroupAsync(AttachmentForCreateDto dto, long Id);
+        ValueTask<Attachment> UploadToUserAsync(AttachmentForCreateDto dto, long Id);
+        ValueTask<Attachment> UpdateAsync(int id, Stream stream);
+        ValueTask<Attachment> CreateAsync(string filePath);
+    }
+}
---
+++ Bazar.Service/Service/AttachmentService.cs
@@ -0,0 +1,14 @@
+﻿using Bazar.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazar.Service.Service
+{
+    public class AttachmentService
+    {
+
+    }
+}
